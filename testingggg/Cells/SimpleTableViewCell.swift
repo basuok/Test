05acc0c417cb0cc091ffa2d303b97114ac872b29
Bassel Isshaq <bassel.isq@icloud.com>
2023-03-09 15:00:58 +0300

    added table view

---
+++ testingggg/Cells/SimpleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SimpleTableViewCell.swift
+//  testingggg
+//
+//  Created by Bassel Isshaq on 09.03.2023.
+//
+
+import UIKit
+
+class SimpleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
